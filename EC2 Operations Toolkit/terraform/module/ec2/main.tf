d0674305a1ac0cc8d51757142243b2d4212cff60
ahmedmohsen5 <a.khalefa555@gmail.com>
2026-08-21 21:24:22 +0300

    edit the security group

--- EC2 Operations Toolkit/terraform/module/ec2/main.tf
+++ EC2 Operations Toolkit/terraform/module/ec2/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "project_ec2" {
   subnet_id = var.public_subnet
   key_name = data.aws_key_pair.get_key.key_name
   associate_public_ip_address = true
-  security_groups = [ var.security_group ]
+  vpc_security_group_ids = [ var.security_group ]
   tags = {
     Name = "${var.project_name}-ec2"
   }
